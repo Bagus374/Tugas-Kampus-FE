4e18cd519a03fc55a63535d19af1157d266533a4
Bagus374 <kursusprojek@gmail.com>
2022-06-04 13:11:05 +0700

    Task Pertemuan 11

--- movie-database/src/components/Hero/Hero.js
+++ movie-database/src/components/Hero/Hero.js
@@ -1,31 +1,49 @@
 /* eslint-disable react-hooks/exhaustive-deps */
+import axios from "axios";
 import { useEffect, useState } from "react";
 import Button from "../ui/Button";
 import { StyledHero, StyledHeroLeft, StyledHeroRight } from "./Hero.styled";
 
 function Hero() {
   const [movie, setMovie] = useState("");
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  const genres = movie && movie.genres.map((genre) => genre.name).join(", ");
+  const idTrailer = movie && movie.videos.results[0].key;
 
-  async function fetchMovie() {
-    const url = "https://www.omdbapi.com/?apikey=fcf50ae6&i=tt2975590";
-    const response = await fetch(url);
-    const data = await response.json();
-    setMovie(data);
+  async function getTrendingMovie() {
+    const URL = `https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`;
+    const response = await axios(URL);
+    return response.data.results[0];
   }
-  useEffect(fetchMovie, []);
-  console.log(movie);
+
+  async function getDetailMovie() {
+    const trendingMovie = await getTrendingMovie();
+    const id = trendingMovie.id;
+    const URL = `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&append_to_response=videos`;
+    const response = await axios(URL);
+    setMovie(response.data);
+  }
+
+  useEffect(getDetailMovie, []);
 
   return (
     <StyledHero>
       <section>
         <StyledHeroLeft>
           <h2>{movie.Title}</h2>
-          <h3>Genre: {movie.Genre}</h3>
-          <p>{movie.Plot}</p>
-          <Button variant="primary">Watch</Button>
+          <h3>{genres}</h3>
+          <p>{movie.overview}</p>
+          <Button
+            as="a"
+            variant="primary"
+            href={`https://www.youtube.com/watch?v=${idTrailer}`}
+            target="_blank"
+          >
+            Watch Movie
+          </Button>
         </StyledHeroLeft>
         <StyledHeroRight>
-          <img src={movie.Poster} alt="placeholder" />
+          <img src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`} alt="placeholder" />
         </StyledHeroRight>
       </section>
     </StyledHero>
--- movie-database/src/pages/movie/TopRated.js
+++ movie-database/src/pages/movie/TopRated.js
@@ -1,7 +1,29 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Hero from "../../components/Hero/Hero";
+import Movies from "../../components/Movies/Movies";
+
 function TopRatedMovie() {
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  const URL = `https://api.themoviedb.org/3/movie/top_rated/?api_key=${API_KEY}`;
+
+  const [movies, setMovies] = useState([]);
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => {
+    getTopRatedMovie();
+  }, []);
+
+  async function getTopRatedMovie() {
+    const response = await axios(URL);
+
+    setMovies(response.data.results);
+  }
+
   return (
     <>
-      <h2>Top Rated</h2>
+      <Hero />
+      <Movies movies={movies} />
     </>
   );
 }
